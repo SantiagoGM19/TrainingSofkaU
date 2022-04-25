2aba02f5f32d3f6eac96bbaf2839481c413a5f52
SantiagoGM19 <santigm1905@gmail.com>
2022-04-24 20:38:25 -0500

    Finalizacion de la implementacion de la logica de colisiones

--- Introduccion al desarrollo de software/TallerEvaluacionJavaScript/main.js
+++ Introduccion al desarrollo de software/TallerEvaluacionJavaScript/main.js
@@ -26,6 +26,9 @@
         this.speed_x = 3;
         this.board = board;
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
 
         this.board.ball = this;
         this.kind = "circle";
@@ -41,6 +44,17 @@
         },
         get height(){
             return this.radius * 2;
+        },
+        collision: function (bar) {
+            //Reaccion a la colision con una barra que recibe como parámetro
+            let relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+            let normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if (this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction = 1;
         }
     }
 })();
@@ -179,8 +193,6 @@ document.addEventListener("keydown", function (ev) {
         ev.preventDefault();
         board.playing = !board.playing;
     }
-    console.log("bar2 " + bar2.toString());
-    console.log("bar1 " + bar1.toString());
 });
 
 board_view.draw();
